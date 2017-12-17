d35ca3f3978fab6dc63e71f04a5e4a72d1e9dca8
TaniaNaOH <tania.naoh83@gmail.com>
2017-12-17 10:36:30 -0600

    avance

--- README.md
+++ README.md
@@ -21,5 +21,7 @@
 3. Cuando hayas terminado tu producto, envía un Pull Request a este repositorio
    (puedes solicitar apoyo de tus profes para este paso).
 
+COMENZANDO COMMITS
+
 > Nota: No olvides que es una buena práctica describir tu proyecto en este
 > archivo `README.md` y también desplegar tu web a Github Pages :smiley:.
--- js/app.js
+++ js/app.js
@@ -1,6 +1,69 @@
-/*
- * Funcionalidad de tu producto
- */
 
-// Puedes hacer uso de la base de datos a través de la variable `data`
-console.log(data);
+// Pintar grafica
+var ctx = document.getElementById("myChart").getContext("2d");
+ var myChart = new Chart (ctx, {
+ 	type: 'bar',
+ 	data: {
+ 		labels: ["AQP 2016-2","AQP 2016-2 Activas","AQP 2016-2 Desertaron"],
+       datasets: [{
+             label: 'Total',
+              data: [15, 7, 8],
+             backgroundColor: [
+               'rgba(153, 102, 255, 0.6)',
+               'rgba(54, 162, 235, 0.6)',
+               'rgba(255, 99, 132, 0.6)',
+             ],
+             borderColor: [
+               'rgba(153, 102, 255, 0.6)',
+               'rgba(54, 162, 235, 0.6)',
+               'rgba(255, 99, 132, 0.6)',
+             ],
+             borderWidth: 1
+         }]
+
+       },
+       options:  {
+           scales: {
+               yAxes: [{
+                   ticks: {
+                       beginAtZero:true
+                   }
+               }]
+        }
+
+      }
+})
+
+var ctx = document.getElementById("myChart2").getContext("2d");
+ var myChart = new Chart (ctx, {
+ 	type: 'bar',
+ 	data: {
+ 		labels: ["AQP 2017-1","AQP 2017-1 Activas","AQP 2017-1 Desertaron"],
+       datasets: [{
+             label: 'Total',
+              data: [15, 8, 7],
+             backgroundColor: [
+               'rgba(153, 102, 255, 0.6)',
+               'rgba(54, 162, 235, 0.6)',
+               'rgba(255, 99, 132, 0.6)',
+             ],
+             borderColor: [
+               'rgba(153, 102, 255, 0.6)',
+               'rgba(54, 162, 235, 0.6)',
+               'rgba(255, 99, 132, 0.6)',
+             ],
+             borderWidth: 1
+         }]
+
+       },
+       options: {
+           scales: {
+               yAxes: [{
+                   ticks: {
+                       beginAtZero:true
+                   }
+               }]
+        }
+
+      }
+})
